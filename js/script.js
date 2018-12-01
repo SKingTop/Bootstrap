14c818d84945124a5604d6ceaf3a6a10b8c4d85c
SKing <secret.king.1996@gmail.com>
2018-12-01 19:46:27 +0600

    01.12.2018/19:46

---
+++ js/script.js
@@ -0,0 +1,36 @@
+		document.getElementById('block2FaceSideButton').onclick = function () {
+			document.getElementById('block2_Rotation').classList.add('activeRot');
+		};
+		document.getElementById('block2BackSideButton').onclick = function () {
+			document.getElementById('block2_Rotation').classList.remove('activeRot');
+		};
+		document.getElementById('block4FaceSideButton').onclick = function () {
+			document.getElementById('block4_Rotation').classList.add('activeRot');
+		};
+		document.getElementById('block4BackSideButton').onclick = function () {
+			document.getElementById('block4_Rotation').classList.remove('activeRot');
+		};
+		document.getElementById('block5FaceSideButton').onclick = function () {
+			document.getElementById('block5_Rotation').classList.add('activeRot');
+		};
+		document.getElementById('block5BackSideButton').onclick = function () {
+			document.getElementById('block5_Rotation').classList.remove('activeRot');
+		};
+		document.getElementById('block3FaceSideButton').onclick = function () {
+			document.getElementById('block3_Rotation').classList.add('activeRot');
+		};
+		document.getElementById('block3BackSideButton').onclick = function () {
+			document.getElementById('block3_Rotation').classList.remove('activeRot');
+		};
+		document.getElementById('block1FaceSideButton').onclick = function () {
+			document.getElementById('block1_Rotation').classList.add('activeRot');
+		};
+		document.getElementById('block1BackSideButton').onclick = function () {
+			document.getElementById('block1_Rotation').classList.remove('activeRot');
+		};
+		document.getElementById('block6FaceSideButton').onclick = function () {
+			document.getElementById('block6_Rotation').classList.add('activeRot');
+		};
+		document.getElementById('block6BackSideButton').onclick = function () {
+			document.getElementById('block6_Rotation').classList.remove('activeRot');
+		};
